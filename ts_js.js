0b057d01fbf4e961ee6a6b52092895fc3461f2f6
Luca <lber0006@student.monash.edu>
2021-09-29 12:33:25 +1000

    worldmap

---
+++ ts_js.js
@@ -0,0 +1,4 @@
+var vg_1 = "tsunami_worldmap.vg.json";
+vegaEmbed("#Worldmap", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
